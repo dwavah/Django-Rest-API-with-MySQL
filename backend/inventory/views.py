59220973880016230d6c2a11fc24e1b31b0f7c26
Daniel Wavamunno <danl.wavah@gmail.com>
2025-03-28 21:33:41 +0300

    updating views so that users see their own items and the items are saved under specific users

--- backend/inventory/views.py
+++ backend/inventory/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Item
+from .serializers import ItemSerializer
 
-# Create your views here.
+class ItemViewSet(viewsets.ModelViewSet):
+    serializer_class = ItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Item.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
