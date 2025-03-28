2295a15d10f58515b50f85cf07cb4003538d05e0
Daniel Wavamunno <danl.wavah@gmail.com>
2025-03-28 22:23:17 +0300

    updating views to add a registration view for users

--- backend/inventory/views.py
+++ backend/inventory/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 from .models import Item
-from .serializers import ItemSerializer
+from .serializers import ItemSerializer, RegisterSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class ItemViewSet(viewsets.ModelViewSet):
     serializer_class = ItemSerializer
@@ -12,4 +14,12 @@ class ItemViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
