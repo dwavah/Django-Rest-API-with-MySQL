8d27619e91d0d845b4a96a94f8d832315470432d
Daniel Wavamunno <danl.wavah@gmail.com>
2025-03-28 22:19:02 +0300

    updating serializers to add users

--- backend/inventory/serializers.py
+++ backend/inventory/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
 from .models import Item
+from django.contrib.auth.models import User
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = ['id', 'user', 'name', 'model', 'cost']
         read_only_fields = ['user']
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        return user
